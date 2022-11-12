be8212c5d19131903f7acdfbde08ca030d96bb38
Vikas Katakdound <76171246+vikas-katakdound@users.noreply.github.com>
2022-11-12 11:32:20 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<time.h>
+
+clock_t end =clock();
+  time_spent+=(double)(end-begin)/(CLOCKS_PER_SEC/1000);
+  printf("The elapsed time is %f seconds \n",time_spent);double time_spent=0.0;
+
+int main()
+{
+  clock_t begin=clock();
+
+  double first, second, temp;
+  printf("Enter first number: ");
+  scanf("%lf", &first);
+  printf("Enter second number: ");
+  scanf("%lf", &second);
+
+  // value of first is assigned to temp
+  temp = first;
+
+  // value of second is assigned to first
+  first = second;
+
+  // value of temp (initial value of first) is assigned to second
+  second = temp;
+
+  // %.2lf displays number up to 2 decimal points
+  printf("\nAfter swapping, first number = %.2lf\n", first);
+  printf("\nAfter swapping, second number = %.2lf", second);
+  clock_t end =clock();
+  time_spent+=(double)(end-begin)/(CLOCKS_PER_SEC/1000);
+  printf("The elapsed time is %f seconds \n",time_spent);
+  return 0;
+}
