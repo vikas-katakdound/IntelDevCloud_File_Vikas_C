41f04cee7a19449bb3b95a61196bd9397f0cee48
Vikas Katakdound <76171246+vikas-katakdound@users.noreply.github.com>
2022-11-12 11:35:51 +0530

    Create ProgramtoAddTwoMatrices2.c

---
+++ ProgramtoAddTwoMatrices2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include<time.h>
+
+double time_spent=0.0;
+
+int main() {
+clock_t begin=clock();
+  int r, c, a[100][100], b[100][100], sum[100][100], i, j;
+  printf("Enter the number of rows (between 1 and 100): ");
+  scanf("%d", &r);
+  printf("Enter the number of columns (between 1 and 100): ");
+  scanf("%d", &c);
+
+  printf("\nEnter elements of 1st matrix:\n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("Enter element a%d%d: ", i + 1, j + 1);
+      scanf("%d", &a[i][j]);
+    }
+
+  printf("Enter elements of 2nd matrix:\n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("Enter element b%d%d: ", i + 1, j + 1);
+      scanf("%d", &b[i][j]);
+    }
+
+  // adding two matrices
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      sum[i][j] = a[i][j] + b[i][j];
+    }
+
+  // printing the result
+  printf("\nSum of two matrices: \n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("%d   ", sum[i][j]);
+      if (j == c - 1) {
+        printf("\n\n");
+      }
+    }
+ clock_t end =clock();
+  time_spent+=(double)(end-begin)/(CLOCKS_PER_SEC/1000);
+  printf("The elapsed time is %f seconds",time_spent);
+  return 0;
+}
